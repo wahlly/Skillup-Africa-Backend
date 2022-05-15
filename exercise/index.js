8b061bbf407da5eb4ed6090783fd088323849b70
Wahllywest <olaifaolawaleh@gmail.com>
2022-05-15 16:24:01 +0100

    Nodejs writeStream

--- exercise/index.js
+++ exercise/index.js
@@ -1,27 +1,51 @@
-  
+const https = require('https');
 const fs = require('fs');
-const fetch = require('node-fetch');
+const crypto = require('crypto')
+const { argv } = require('process')
+// const fetch = require('node-fetch');
 const path = require('path');
 const url = 'https://coderbyte.com/api/challenges/json/age-counting';
 
-//fetching api... 
-let result;
-const getData = (async () => {
-    const response = await fetch(`${url}`)
-    const data = await response.json();
-    result = data;
-    console.log(result)
 
-    for(let i = 0; i < result.length; i++) {
-        if(result.data[age] == 32) {
-            fs.createWriteStream(path.join(__dirname, 'output.txt'))
-        }
-    }
+https.get(url, (res) => {
 
+    let data = ''
+    res.on('data', (d) => {
+        data += d.toString()
+    })
     
-    // fs.writeFile(path.join(__dirname, './result', 'output.txt'), JSON.stringify(result), (err) => {
-    //     if (err) {
-    //         console.log(err)
-    //     }
-    // })
-})();
+    let sortedArr = []
+    res.on('end', async () => {
+        const body = JSON.parse(data).data
+        const edittedArray = body.split(',')
+
+        edittedArray.forEach((val, index) => {
+            if(!val.includes(' age=')) {
+                keyVal = val.includes('key=') ? val.replace('key=', '').trim() : val.replace(' key=', '').trim()
+                let ageVal = edittedArray[index + 1].replace(' age=', '').trim()
+
+                if(Number(ageVal) == 32) {
+                    sortedArr.push({
+                        [keyVal]: ageVal
+                    })
+                }
+            }
+        })
+        console.log(sortedArr)
+
+        const writeStream = fs.createWriteStream('output.txt')
+        const streamingPath = writeStream.path
+        sortedArr.forEach((entry, index) => {
+            index === 0 ? writeStream.write(`${Object.keys(entry)}`) : writeStream.write(`\n${Object.keys(entry)}`)
+        })
+
+            const fileBuffer = fs.readFileSync('output.txt')
+            const hash = await crypto.createHash('sha256')
+            hash.update(fileBuffer)
+            const hashString = hash.digest('hex')
+            console.log(`${hashString} ${streamingPath}`)
+    })
+
+}).on('error', (err) => {
+    console.log(err)
+})
